cf5e9916bb3458894e7e4902f0d0d86736e9837f
kaka120 <knowledgebase006@gmail.com>
2022-10-12 19:58:20 +0530

    commit

--- controller/LoginRegistrationController.js
+++ controller/LoginRegistrationController.js
@@ -6,13 +6,10 @@ module.exports =  function  register(req, res, next) {
     
     let result = userService.LoginRegsistration(req.body)
     result.then(function (result_set) {
-        //console.log("Promise Resolved");
-        //console.log(result_set)
         if(result_set.status===201)
             res.status(201).send(result_set.message)
         
     }).catch(function (result_set) {
-        //console.log(result_set)
         if(result_set.status===409)
             res.status(409).send(result_set.message)
             if(result_set.status===500)
