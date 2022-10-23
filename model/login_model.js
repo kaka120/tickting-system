5166c83f749d37b55e93ee80dca3e25ebc350c41
kaka120 <knowledgebase006@gmail.com>
2022-10-23 12:11:51 +0530

    commit

--- model/login_model.js
+++ model/login_model.js
@@ -1,12 +1,14 @@
-const Schema = require('../helpers/model_library_support');
+const Schema = require("../helpers/model_library_support");
 
 const schema = new Schema.Schema({
-    username: { type: String, unique: true, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    hash: { type: String, required: true },
+  userName: { type: String, unique: true, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  hash: { type: String, required: true },
+  userId: { type: String, required: true, default: null },
+  accountCreationDate: { type: Date, default: Date.now },
 });
 
-schema.set('toJSON', { virtuals: true });
+schema.set("toJSON", { virtuals: true });
 
-module.exports = Schema.mongoose.model('registration_table', schema);
+module.exports = Schema.mongoose.model("registration_table", schema);
