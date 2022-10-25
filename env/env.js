9352aad99354043e97c1494e3e2a9fc45b396398
kaka120 <knowledgebase006@gmail.com>
2022-10-25 21:14:59 +0530

    File removed .env

--- env/env.js
+++
@@ -1,6 +0,0 @@
-
-const dotenv = require('dotenv');
-const process1 = dotenv.config({path:'./env/.env'})
- /* import part of connection */
-module.exports = process1;
-/* import part of connection */
